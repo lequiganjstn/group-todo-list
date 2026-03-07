fb12ba180d1b4cf7d15a94b8065ae7ba51207aee
Plum-afk <24103848@usc.edu.ph>
2026-03-07 10:35:24 +0800

    Initial commit

---
+++ group-todo-list/Models/ToDoList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace group_todo_list.Models
+{
+    internal class ToDoList
+    {
+    }
+}
---
+++ group-todo-list/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace group_todo_list.ViewModels
+{
+    internal class MainViewModel
+    {
+    }
+}
